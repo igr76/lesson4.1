457ffaab63dde903415e8fc940381db2ee94e3bd
igr1 <dfrgth>
2022-11-19 21:16:36 +1000

    4.1 proseed

--- src/main/resources/liquibase/scripts/lesson-three.sql
+++ src/main/resources/liquibase/scripts/lesson-three.sql
@@ -1,5 +1,5 @@
 – liquibase formatted sql
 
-– changeset jrembo:1
+– changeset jrembo:2
 CREATE INDEX search_for_name ON students (name);
 CREATE INDEX search_for_name_color ON faculty (name, color);
