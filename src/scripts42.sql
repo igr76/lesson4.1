9de905d3802acc04d11375a6ab42b4e273bbe392
igr1 <dfrgth>
2022-11-22 07:37:24 +1000

    4.1 proseed

--- src/scripts42.sql
+++ src/scripts42.sql
@@ -1,11 +1,11 @@
-ALTER TABLE students (
+ALTER TABLE students
 
     AND CONSTRAINT unique_name UNIQUE (name),
     ADD CHECK ( age >=16 ),
     ALTER COLUMN name SET NOT NULL ,
     ALTER COLUMN age SET DEFAULT 20,
 
-)
-ALTER TABLE faculty(
+
+ALTER TABLE faculty
     ADD CONSTRAINT unique_name_and_color UNIQUE (name,color);
-)
+
